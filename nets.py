2f93e1e352efb8a3b9ba000c75accd8df3533ee6
Yuta Koreeda <koreyou@mac.com>
2019-01-04 16:54:43 +0900

    Added dropout to features

--- nets.py
+++ nets.py
@@ -1,8 +1,28 @@
+import copy
+
 import chainer
 import chainer.functions as F
 import numpy as np
+import scipy.sparse as sp
 from chainer import initializers
 from chainer import reporter
+from chainer.utils.sparse import CooMatrix
+
+from graphs import to_chainer_sparse_variable
+
+
+def sparse_to_gpu(x, device):
+    x.data.data = chainer.backends.cuda.to_gpu(x.data.data, device=device)
+    x.row = chainer.backends.cuda.to_gpu(x.row, device=device)
+    x.col = chainer.backends.cuda.to_gpu(x.col, device=device)
+    return x
+
+
+def sparse_to_cpu(x):
+    x.data.data = chainer.backends.cuda.to_cpu(x.data.data)
+    x.row = chainer.backends.cuda.to_cpu(x.row)
+    x.col = chainer.backends.cuda.to_cpu(x.col)
+    return x
 
 
 class TextGCN(chainer.Chain):
@@ -11,16 +31,22 @@ class TextGCN(chainer.Chain):
         n_class = np.max(labels) + 1
         initializer = initializers.HeUniform()
         with self.init_scope():
-            self.gconv1 = GraphConvolution(feat_size, feat_size, noweight=True)
+            self.gconv1 = GraphConvolution(adj.shape[1], feat_size)
             self.gconv2 = GraphConvolution(feat_size, n_class)
             self.input_repr = chainer.Parameter(
                 initializer, (adj.shape[0], feat_size))
+        # This Variable will not be updated because require_grad=False
+        self.input = to_chainer_sparse_variable(
+            sp.identity(adj.shape[1]))
         self.adj = adj
         self.labels = labels
         self.dropout = dropout
 
     def _forward(self):
-        h = F.relu(self.gconv1(self.input_repr, self.adj))
+        # deep copy object, shallow copy internal arrays
+        x = copy.deepcopy(self.input)
+        x.data = F.dropout(x.data, self.dropout)
+        h = F.relu(self.gconv1(x, self.adj))
         h = F.dropout(h, self.dropout)
         out = self.gconv2(h, self.adj)
         return out
@@ -56,26 +82,21 @@ class TextGCN(chainer.Chain):
         return out.data
 
     def to_gpu(self, device=None):
-        adj = self.adj
-        adj.data.data = chainer.backends.cuda.to_gpu(adj.data.data, device=device)
-        adj.row = chainer.backends.cuda.to_gpu(adj.row, device=device)
-        adj.col = chainer.backends.cuda.to_gpu(adj.col, device=device)
-        self.adj = adj
+        self.adj = sparse_to_gpu(self.adj, device=device)
+        self.input = sparse_to_gpu(self.input, device=device)
         self.labels = chainer.backends.cuda.to_gpu(self.labels, device=device)
         return super(TextGCN, self).to_gpu(device=device)
 
     def to_cpu(self):
-        adj = self.adj
-        adj.data.data = chainer.backends.cuda.to_cpu(adj.data.data)
-        adj.row = chainer.backends.cuda.to_cpu(adj.row)
-        adj.col = chainer.backends.cuda.to_cpu(adj.col)
+        self.adj = sparse_to_cpu(self.adj)
+        self.input = sparse_to_cpu(self.input)
         self.labels = chainer.backends.cuda.to_cpu(self.labels)
         return super(TextGCN, self).to_cpu()
 
 
 class GraphConvolution(chainer.Link):
-    def __init__(self, in_size, out_size=None, noweight=False,
-                 nobias=False, initialW=None, initial_bias=None):
+    def __init__(self, in_size, out_size=None, nobias=False, initialW=None,
+                 initial_bias=None):
         super(GraphConvolution, self).__init__()
 
         if out_size is None:
@@ -83,12 +104,9 @@ class GraphConvolution(chainer.Link):
         self.out_size = out_size
 
         with self.init_scope():
-            if noweight:
-                self.W = None
-            else:
-                if initialW is None:
-                    initialW = initializers.HeUniform()
-                self.W = chainer.Parameter(initialW, (in_size, out_size))
+            if initialW is None:
+                initialW = initializers.HeUniform()
+            self.W = chainer.Parameter(initialW, (in_size, out_size))
             if nobias:
                 self.b = None
             else:
@@ -98,7 +116,9 @@ class GraphConvolution(chainer.Link):
                 self.b = chainer.Parameter(bias_initializer, out_size)
 
     def __call__(self, x, adj):
-        if self.W is not None:
+        if isinstance(x, chainer.utils.CooMatrix):
+            x = F.sparse_matmul(x, self.W)
+        else:
             x = F.matmul(x, self.W)
         output = F.sparse_matmul(adj, x)
 
